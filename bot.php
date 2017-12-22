0b707f9e6e406160dcb60d5bc94cfca0c6f22e5c
promphan <34728074+promphan@users.noreply.github.com>
2017-12-22 15:36:25 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -45,7 +45,3 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK";
-
-<?php
-$outputText = new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder("Eiffel Tower", "Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France", 48.858328, 2.294750);
-$response = $bot->replyMessage($event->getReplyToken(), $outputText);
