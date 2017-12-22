36fb52f7c6a1dcb182178d509264f8104b1f5c91
promphan <34728074+promphan@users.noreply.github.com>
2017-12-22 15:48:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -59,3 +59,7 @@ foreach ($events as $event) {
     
   }
 }  
+<?php
+$img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
+$outputText = new LINE\LINEBot\MessageBuilder\ImageMessageBuilder($img_url, $img_url);
+$response = $bot->replyMessage($event->getReplyToken(), $outputText);
