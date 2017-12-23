9228035201567c792e0c0a456f1704e22b2315ad
promphan <34728074+promphan@users.noreply.github.com>
2017-12-23 08:34:06 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -28,7 +28,14 @@ if (!is_null($events['events'])) {
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
 			];
-			$post = json_encode($data);
+			{
+    "type": "location",
+    "title": "my location",
+    "address": "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
+    "latitude": 35.65910807942215,
+    "longitude": 139.70372892916203
+}
+		        $post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 			$ch = curl_init($url);
